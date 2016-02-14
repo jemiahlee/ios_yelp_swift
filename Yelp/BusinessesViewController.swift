03dcbe921959cfcc7a1676b70bc4acb43a30b4fd
Jeremiah Lee <jeremiah.lee@gmail.com>
2016-02-14 09:47:59 -0800

    Search is basically working.

--- Yelp/BusinessesViewController.swift
+++ Yelp/BusinessesViewController.swift
@@ -8,35 +8,27 @@
 
 import UIKit
 
-class BusinessesViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+class BusinessesViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, UISearchBarDelegate {
     
     @IBOutlet weak var listingTableView: UITableView!
     
     var businesses: [Business]!
+    var searchBar: UISearchBar!
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        let searchBar = UISearchBar()
+        searchBar = UISearchBar()
         searchBar.sizeToFit()
+        searchBar.delegate = self
         navigationItem.titleView = searchBar
-
 
         listingTableView.delegate = self
         listingTableView.dataSource = self
         listingTableView.estimatedRowHeight = 100
         listingTableView.rowHeight = UITableViewAutomaticDimension
         
-        Business.searchWithTerm("Thai", completion: { (businesses: [Business]!, error: NSError!) -> Void in
-            self.businesses = businesses
-            
-            for business in businesses {
-                print(business.name!)
-                print(business.address!)
-            }
 
-            self.listingTableView.reloadData()
-        })
 
 
 /* Example of Yelp search with more search options specified
@@ -50,8 +42,23 @@ class BusinessesViewController: UIViewController, UITableViewDelegate, UITableVi
         }
 */
         
+        performSearch("")
+        
     }
 
+    func performSearch(term: String){
+        Business.searchWithTerm(term, completion: { (businesses: [Business]!, error: NSError!) -> Void in
+            self.businesses = businesses
+            
+            for business in businesses {
+                print(business.name!)
+                print(business.address!)
+            }
+            
+            self.listingTableView.reloadData()
+        })
+    }
+    
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCellWithIdentifier("ListingCell", forIndexPath: indexPath) as! ListingCell
         cell.listing = businesses[indexPath.row]
@@ -68,5 +75,23 @@ class BusinessesViewController: UIViewController, UITableViewDelegate, UITableVi
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
-
+    
+    func searchBarTextDidBeginEditing(searchBar: UISearchBar) {
+        searchBar.showsCancelButton = true
+    }
+    
+    func searchBarCancelButtonClicked(searchBar: UISearchBar) {
+        searchBar.showsCancelButton = false
+        searchBar.text = ""
+        searchBar.resignFirstResponder()
+    }
+    
+    func searchBar(searchBar: UISearchBar, textDidChange searchText: String) {
+        // When there is no text, filteredData is the same as the original data
+        if searchText.isEmpty {
+            performSearch("")
+        } else {
+            performSearch(searchText)
+        }
+    }
 }
